05713e8c141eef7d987643ad8debdda5f633eb41
riana <cascade.lebehy@gmail.com>
2026-03-11 16:53:47 +0100

    ajout et test des affichages de vector ex1

---
+++ exercice1.cpp
@@ -0,0 +1,36 @@
+#include <vector>
+#include <iostream>
+#include "exercice1.h"
+
+using namespace std;
+
+int main(){
+    vector<int> vect; //définition du vecteur
+    vect.resize(3); //redimensionnement du vecteur
+    vect = {1,2,3};
+
+    //Affichage des valeurs du tableau avec des indices
+    cout << "-- Affichage des valeurs du tableau avec des indices --" << endl ;
+    for (int i = 0; i < 3; i++ ){
+        cout << vect[i] << endl;
+    }
+
+    //Affichage des valeurs du tableau avec une boucle sur une plage de nb
+    cout << "-- Affichage des valeurs du tableau avec une boucle sur une plage de nb --" << endl ;
+    for (int k : vect){
+        cout << k << endl;
+    }
+
+    //Affichage d'une plage
+    cout << "-- Affichage des valeurs d'une plage --" << endl ;
+    for (int k : vect){
+        cout << k << endl;
+    }
+
+    //Affichage des valeurs à l'aide d'un itérateur
+    cout << "-- Affichage des valeurs d'un itérateur --" << endl ;
+    for(std::vector<int>::iterator it = vect.begin();it != vect.end(); it++){
+        cout << *it << endl;
+    }
+    return 0;
+}
