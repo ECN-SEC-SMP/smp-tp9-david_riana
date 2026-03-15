f2dff32360610fae1ab178787b271a2b896529bd
David Prospérin <32713756+dprosperin@users.noreply.github.com>
2026-03-15 12:28:01 +0100

    Fix: ajoute_evenement

--- exercice4.cpp
+++ exercice4.cpp
@@ -1,39 +1,75 @@
+#include <cassert>
 #include <map>
 #include <list>
 #include <string>
 #include <iostream>
 
-
 using namespace std;
+using evenement_t = list<string>;
+using calendrier_t = map<int, evenement_t>;
 
-//Il y a des chances que les deux méthodes utilisées soient équivalentes...
+bool ajoute_evenement(calendrier_t &cal, int annee, string intitule) {
+    auto it = cal.find(annee);
+    evenement_t::iterator it_event;
 
-void ajoute_evenement(std::map<int,list<string>> dico,int date,string intitule){
-    if(dico.find(date) != dico.end()){
-        dico[date].push_back(intitule);
+    if (it == cal.end()) {
+        list<string> evenement = {intitule};
+        cal[annee] = evenement;
+        return true;
     }
-    else{
-        dico.insert({date,{intitule}});
-    }  
+
+    it_event = find(cal[annee].begin(), cal[annee].end(), intitule);
+
+    if (it_event == cal[annee].end()) {
+        cal[annee].push_back(intitule);
+        return true;
+    }
+    return false;
 }
 
-void affiche_liste(list<string> lst){ //Fonction qui permet l'affichage des listes (et donc permet de tester notre map)
+void affiche_liste(evenement_t &lst) {
     cout << '[';
-    for(std::list<string>::iterator it = lst.begin(); it != lst.end(); it++){
+    for(auto it = lst.begin(); it != lst.end(); it++){
         cout << *it;
+
+        if (next(it) != lst.end()) cout << ", ";
     }
     cout << ']' << endl;
 }
 
-int main(){
-    std::map<int,list<string>> dico_event;
-    cout << "-- TEST DE L'AJOUT D'ÉVÈNEMENT DANS UN DICTIONNAIRE --" << endl ;
-    ajoute_evenement(dico_event,2018,"Victoire de la CDM");
-      for (std::map<int,list<string>>::iterator it = dico_event.begin(); it!=dico_event.end(); ++it){
-        std::cout << it->first << " => ";
+void affiche_calendrier(calendrier_t &cal) {
+    cout << "-- Calendrier --" << endl;
+    for(auto it = cal.begin(); it != cal.end(); it++) {
+        cout << "en " << it->first << " : ";
         affiche_liste(it->second);
-      }
-    
+    }
+    cout << "----------------" << endl;
+}
 
+int main(){
+    calendrier_t calendrier;
+
+    cout << "-- TEST DE L'AJOUT D'ÉVÈNEMENT DANS UN DICTIONNAIRE --" << endl ;
+
+    assert(ajoute_evenement(calendrier,2018,"Victoire de la CDM") && "L'évènement doit être ajouté dans le calendrier");
+    assert(calendrier.count(2018) == 1 && "L'année 2018 doit être présente dans le calendrier");
+    assert(find(calendrier[2018].begin(), calendrier[2018].end(), "Victoire de la CDM") != calendrier[2018].end()
+        && "L'évènement doit être présent dans le calendrier");
+
+    // On ajoute un autre evenement pour la même année
+    assert(ajoute_evenement(calendrier,2018,"panthéonisation de Simone Veil et de son mari Antoine") && "L'évènement doit être ajouté dans le calendrier");
+    assert(calendrier.count(2018) == 1 && "L'année 2018 doit être présente dans le calendrier");
+    assert(find(calendrier[2018].begin(), calendrier[2018].end(), "panthéonisation de Simone Veil et de son mari Antoine") != calendrier[2018].end()
+        && "L'évènement doit être présent dans le calendrier");
+
+    // On ajoute un évènement qui existe déjà
+    assert(
+        !ajoute_evenement(calendrier,2018,"Victoire de la CDM") &&
+        "L'évènement ne doit pas être ajouté dans le calendrier");
+    assert(calendrier.count(2018) == 1 && "L'année 2018 doit être présente dans le calendrier");
+    assert(count(calendrier[2018].begin(), calendrier[2018].end(), "Victoire de la CDM") == 1
+        && "L'évènement doit être présent une seule fois dans le calendrier");
+
+    affiche_calendrier(calendrier);
     return 0;
 }
